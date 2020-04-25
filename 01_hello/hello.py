3d454d8a3cf2b236a120fb0377cfc948cdb46346
Andy Fallon <andy.m.fallon@gmail.com>
2020-04-25 18:30:06 +0100

    add docstrings

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: Grey Cat
+Purpose: Say hello
+"""
 
 import argparse
 
 
 def get_args():
-    parser =  argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    """Get the command line arguemnts"""
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
     return parser.parse_args()
 
 
 def main():
+    """Make a jazz noise here"""
+
     args = get_args()
     print('Hello, ' + args.name + '!')
 
+
 if __name__ == '__main__':
     main()
-
